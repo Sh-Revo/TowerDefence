ebd9c77aedb0b83a2845c1a8d02bfb7e37805ac6
Sh-Revo <shandov@i.ua>
2022-06-20 14:32:36 +0300

    Create project, add ground, start, end, enemy

---
+++ TowerDefence/Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform _target;
+    private int _wayPointIndex = 0;
+
+    private void Start()
+    {
+        _target = Waypoints.points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 direction = _target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, _target.position) <= 0.4f)
+        {
+            GetNextWayPoint();
+        }
+    }
+
+    void GetNextWayPoint()
+    {
+        if (_wayPointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        _wayPointIndex++;
+        _target = Waypoints.points[_wayPointIndex];
+    }
+}
