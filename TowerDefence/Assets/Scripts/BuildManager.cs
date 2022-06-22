2153b743f0acab5d2ccd4f40d1733f5768b9b470
Sh-Revo <shandov@i.ua>
2022-06-22 18:34:09 +0300

    add building

---
+++ TowerDefence/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+    public GameObject standartTurretPrefab;
+
+    private GameObject turretToBuild;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.Log("More than 1 BuildManager in scene!");
+            return;
+        }
+        instance = this;
+    }
+
+    private void Start()
+    {
+        turretToBuild = standartTurretPrefab;
+    }
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
---
+++ TowerDefence/Assets/Scripts/Node.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    public Vector3 positionOffset;
+    private Renderer rend;
+    private Color startColor;
+    private GameObject turret;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+
+    private void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            Debug.Log("Cant build");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+    }
+
+
+}
